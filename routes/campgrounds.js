9fe3dc2aa848eeb7e75ef64fece8cebd5128761f
Sanket  Singh <sanketsingh@Sankets-MacBook-Air.local>
2017-12-27 14:37:52 +0530

    Campground associated with user

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,11 +15,15 @@ router.get("/", function (req, res) {
     });
 });
 
-router.post("/", function (req, res) {
+router.post("/", isLoggedIn, function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var newCampground = {name: name, image: image, description: desc,author: author};
     // Create a new campground with the database
     Campground.create(newCampground, function (err, newlyCreated) {
         if (err) {
@@ -33,7 +37,7 @@ router.post("/", function (req, res) {
 });
 
 // Show form to create new campground
-router.get("/new", function (req, res) {
+router.get("/new", isLoggedIn, function (req, res) {
     res.render("campgrounds/new");
 });
 
@@ -46,8 +50,9 @@ router.get("/:id", function (req, res) {
         }
     })
 });
-function isLoggedIn(req,res,next) {
-    if(req.isAuthenticated()){
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
         return next();
     }
     res.redirect("/login");
