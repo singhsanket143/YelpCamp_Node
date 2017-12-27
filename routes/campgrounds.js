302ad16d8673b7a2a2f0824b1bf8dea32ad378f7
Sanket  Singh <sanketsingh@Sankets-MacBook-Air.local>
2017-12-27 19:11:15 +0530

    dalete added

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -74,6 +74,16 @@ router.put("/:id",function (req,res) {
         }
     })
 });
+// Destroy
+router.delete("/:id",function (req,res) {
+    Campground.findByIdAndRemove(req.params.id,function (err) {
+        if(err){
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
